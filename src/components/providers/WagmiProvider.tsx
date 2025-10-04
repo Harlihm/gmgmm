886da157bc9fd688770b98e81b290c44c338f1a4
Harlihm <107759287+Harlihm@users.noreply.github.com>
2025-10-04 20:50:36 +0100

    m

--- src/components/providers/WagmiProvider.tsx
+++ src/components/providers/WagmiProvider.tsx
@@ -1,13 +1,12 @@
 import { createConfig, http, WagmiProvider } from "wagmi";
-import { base, degen, mainnet, optimism, unichain } from "wagmi/chains";
+import { base, degen, mainnet, unichain } from "wagmi/chains";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { farcasterFrame } from "@farcaster/frame-wagmi-connector";
 
 export const config = createConfig({
-  chains: [base, optimism, mainnet, degen, unichain],
+  chains: [base, mainnet, degen, unichain],
   transports: {
     [base.id]: http(),
-    [optimism.id]: http(),
     [mainnet.id]: http(),
     [degen.id]: http(),
     [unichain.id]: http(),
